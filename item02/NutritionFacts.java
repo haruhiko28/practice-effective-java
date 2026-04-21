782dcf59e3d48bb6b1ffd7afc4cf8d92c662bed0
haruhiko28 <not.hisano.soccer@gmail.com>
2026-04-21 23:06:12 +0900

    add 20260421

--- item02/NutritionFacts.java
+++ item02/NutritionFacts.java
@@ -21,4 +21,26 @@ public class NutritionFacts {
         this(servingSize, servings, calories, 0);
     }
 
+    public NutritionFacts(int servingSize, int servings, 
+        int calories, int fat
+    ) {
+        this(servingSize, servings, calories, fat, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings, 
+        int calories, int fat, int sodium
+    ) {
+        this(servingSize, servings, calories, fat, sodium, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings, 
+        int calories, int fat, int sodium, int carbohydrate
+    ) {
+        this.servingSize = servingSize;
+        this.servings = servings;
+        this.calories = calories;
+        this.fat = fat;
+        this.sodium = sodium;
+        this.carbohydrate = carbohydrate;
+    }
 }

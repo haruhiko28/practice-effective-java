917d7f3c095fc512d30c1fd7cae222f8b09d7e39
haruhiko28 <not.hisano.soccer@gmail.com>
2026-04-25 23:44:05 +0900

    add 20260425

--- item02/NutiritionFactsBuilds.java
+++ item02/NutiritionFactsBuilds.java
@@ -1,50 +1,54 @@
-package item02;
+    package item02;
 
-// Builder Pattern
-public class NutiritionFactsBuilds {
-    private final int servingSize;
-    private final int servings;
-    private final int calories;
-    private final int fat;
-    private final int sodium;
-    private final int carbohydrate;
-
-    public static class Builder {
-        // Required parameters
-        private final int servingSize;
+    // Builder Pattern
+    public class NutiritionFactsBuilds {
+        private final int servingSize; // immutable
         private final int servings;
+        private final int calories;
+        private final int fat;
+        private final int sodium;
+        private final int carbohydrate;
 
-        // Optional parameters - initalized to default values
-        private int calories = 0;
-        private int fat = 0;
-        private int sodium = 0;
-        private int carbohydrate = 0;
+        public static class Builder { // to build instances
+            // Required parameters
+            private final int servingSize;
+            private final int servings;
 
-        public Builder(int servingSize, int servings) {
-            this.servingSize = servingSize;
-            this.servings = servings;
+            // Optional parameters - initalized to default values
+            private int calories = 0;
+            private int fat = 0;
+            private int sodium = 0;
+            private int carbohydrate = 0;
+
+            public Builder(int servingSize, int servings) {
+                this.servingSize = servingSize;
+                this.servings = servings;
+            }
+
+            public Builder calories(int val)
+                {calories = val; return this; }
+            public Builder fat(int val)
+                {fat = val; return this;}
+            public Builder sodium(int val)
+                {sodium = val; return this;}
+            public Builder carbohydrate(int val)
+                {carbohydrate = val; return this;}
+            
+            public NutiritionFactsBuilds build() {
+                return new NutiritionFactsBuilds(this);
+            }
         }
-
-        public Builder calories(int val)
-            {calories = val; return this; }
-        public Builder fat(int val)
-            {fat = val; return this;}
-        public Builder sodium(int val)
-            {sodium = val; return this;}
-        public Builder carbohydrate(int val)
-            {carbohydrate = val; return this;}
+        private NutiritionFactsBuilds(Builder builder) {
+            servingSize = builder.servingSize;
+            servings = builder.servings;
+            calories = builder.calories;
+            fat = builder.fat;
+            sodium = builder.sodium;
+            carbohydrate = builder.carbohydrate;
+        }
         
-        public NutiritionFactsBuilds build() {
-            return new NutiritionFactsBuilds(this);
-        }
     }
-    private NutiritionFactsBuilds(Builder builder) {
-        servingSize = builder.servingSize;
-        servings = builder.servings;
-        calories = builder.calories;
-        fat = builder.fat;
-        sodium = builder.sodium;
-        carbohydrate = builder.carbohydrate;
-    }
-    
-}
+
+// fuluent API
+// NutiritionFactsBuilds cocaCola = new NutiritionFactsBuilds.Builder(240, 8).calories(100).sodium(35).carbohydrate(27).build();
+

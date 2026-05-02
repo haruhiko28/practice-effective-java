690fd126cabd03e6940a401b1a9f9ff5536fe764
haruhiko28 <not.hisano.soccer@gmail.com>
2026-05-02 16:13:10 +0900

    finish item03

--- item02/Elvis.java
+++ item02/Elvis.java
@@ -1,8 +1,26 @@
 package item02;
 
-public class Elvis {
-    public static final Elvis INSTANCE = new Elvis();
-    private Elvis() {}
+// public class Elvis {
+//     public static final Elvis INSTANCE = new Elvis();
+//     private Elvis() {}
+
+//     public void leaveTheBuilding() {}
+// }
+
+// public class Elvis {
+//     private static final Elvis INSTANCE = new Elvis();
+//     private Elvis() {}
+//     public static Elvis getInstance() {return INSTANCE;}
+
+//     public void leaveTheBuilding() {}
+
+//     private Object readResolve() {
+//         return INSTANCE;
+//     }
+// }
+
+public enum Elvis {
+    INSTANCE;
 
     public void leaveTheBuilding() {}
 }

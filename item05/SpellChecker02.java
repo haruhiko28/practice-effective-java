da2c44de3e364db71416e5c8cc3df8e2e0942a9b
haruhiko28 <not.hisano.soccer@gmail.com>
2026-05-06 23:58:49 +0900

    add 20260506

---
+++ item05/SpellChecker02.java
@@ -0,0 +1,16 @@
+package item05;
+
+import java.util.List;
+
+public class SpellChecker02 {
+    private final Lexicon dictionary = new Lexicon();
+
+    private SpellChecker02() {}
+    public static SpellChecker02 INSTANCE = new SpellChecker02();
+
+    public boolean isValid(String word) {return true;}
+    public List<String> suggestions(String typo) {return List.of();}   
+}
+
+class Lexicon {
+}

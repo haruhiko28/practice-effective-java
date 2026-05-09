b3ce9606a010ebf9e3dae8d0fe076651484aa47f
haruhiko28 <not.hisano.soccer@gmail.com>
2026-05-09 22:08:27 +0900

    add 20260509

--- item05/SpellChecker03.java
+++ item05/SpellChecker03.java
@@ -1,5 +1,17 @@
 package item05;
 
+import java.util.List;
+import java.util.Objects;
+
 public class SpellChecker03 {
     private final Lexicon dictionary;    
+
+    public SpellChecker03(Lexicon dictionary) {
+        this.dictionary = Objects.requireNonNull(dictionary);
+    }
+
+    public boolean isValid(String word) {return true;}
+    public List<String> suggestions(String typo) {return List.of();}
 }
+
+// Mosaic create(Supplier<? extends Tile> tileFactory){....}

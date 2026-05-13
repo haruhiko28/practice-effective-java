0a429191ace4c66c65c4dc86d4d887a645adb484
haruhiko28 <not.hisano.soccer@gmail.com>
2026-05-13 22:27:41 +0900

    add 20260513

--- item06/Item06.java
+++ item06/Item06.java
@@ -3,4 +3,10 @@ package item06;
 public class Item06 {
     String s1 = new String("bikini");
     String s2 = "bikini";
+
+    static boolean isRomanNumeral(String s){
+        return s.matches("^(?=.)M*(C[MD]|D?C{0,3})"
+            +"(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$"
+        );
+    }
 }

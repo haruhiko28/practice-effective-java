b35c309de4922fee85330368ce5908f5915fc126
haruhiko28 <not.hisano.soccer@gmail.com>
2026-06-22 23:46:55 +0900

    add 20260622

---
+++ item08/Adult.java
@@ -0,0 +1,9 @@
+package item08;
+
+public class Adult {
+    public static void main(String[] args) {
+        try (Room myRoom = new Room(7)){
+            System.out.println("Goodbye");
+        }
+    }
+}
